957cd05113785b564acb639f110548a914fae6b7
haris <haris@LAPTOP-87SVGSB0>
2019-11-24 18:47:03 +0530

    Day 2 - Class room exercises.

---
+++ src/main/java/day2/classroom/array/FindSecondLargest.java
@@ -0,0 +1,36 @@
+package day2.classroom.array;
+
+import java.util.Arrays;
+
+public class FindSecondLargest {
+
+	
+	public static void main(String[] args) {
+
+		// Here is the input
+		int[] data = {3,2,11,4,6,7};
+		int dataLen = data.length;
+		System.out.println("Array length is "+dataLen);
+		Arrays.sort(data);
+		System.out.println(data[dataLen-2]);
+		
+		
+		//System.out.println("Second largest number is "+dataLen[1]);
+		/*
+		 Pseudo Code:
+		 1) Arrange the array in ascending order
+		 2) Pick the last but one and print
+		 */
+		
+		
+		
+		
+		
+		
+		
+		// Print the second largest number	
+		
+		
+	}
+
+}
